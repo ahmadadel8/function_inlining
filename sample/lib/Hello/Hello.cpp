4f399a3f26c6eb9007c58273ce4e7dbed302f433
ahmadadel8 <ahmadadelattia@gmail.com>
2021-04-27 22:39:00 +0200

    getArgumentList

--- sample/lib/Hello/Hello.cpp
+++ sample/lib/Hello/Hello.cpp
@@ -42,8 +42,8 @@ struct Hello :  public FunctionPass
 		errs().write_escaped(F.getName())<< "arguments are: ";
 		Function *func = &F;
 		//CallBase value;
-		for (unsigned AggArgIdx = 0; AggArgIdx < func->arg_size(); AggArgIdx++)
-			errs().write_escaped(F.getArgOperand(AggArgIdx))<<"\n";
+		for (Function::arg_iterator ArgIdx = func->arg_begin(), ArgEnd= func->arg_end(); ArgIdx !=ArgEnd; ++ArgIdx)
+			errs().write_escaped(func->getArgumentList(ArgIdx))<<"\n";
 		for (Function::iterator blk =func->begin(), blk_e=func->end(); blk!=blk_e; ++blk){
 			errs() << "Basic block (name=" << blk->getName() << ") has " <<blk->size() << "instructions which are:\n";
 		for(BasicBlock::iterator i=blk->begin(), e=blk->end(); i!=e; ++i)
