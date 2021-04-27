b0ca660419f2b098ced1f753568ca6ecf1b314aa
ahmadadel8 <ahmadadelattia@gmail.com>
2021-04-27 23:14:30 +0200

    include "llvm/IR/Instructions.h

--- sample/lib/Hello/Hello.cpp
+++ sample/lib/Hello/Hello.cpp
@@ -16,7 +16,7 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/IR/InstrTypes.h"
+#include "llvm/IR/Instructions.h"
 
 using namespace llvm;
 
