4f58d67caee4b9afa6006fb9337d7a46cc555e8c
ahmadadel8 <ahmadadelattia@gmail.com>
2021-04-29 01:19:02 +0200

    testtt

--- sample/lib/Hello/Hello.cpp
+++ sample/lib/Hello/Hello.cpp
@@ -63,7 +63,7 @@ struct Hello :  public FunctionPass
 								{
 								constArg=ConstantInt::get(IntegerType::get(V->getContext(),32), (uint64_t)V);
 								errs()<<"testing";
-								errs()<<*constArg;
+								errs()<<*V<<*constArg<<'\n';
 								/*calledFunc=callInst->getCalledFunction();
 								errs()<< "Function ";
 								errs().write_escaped(calledFunc->getName())<<  " is called with actual arguments ";
