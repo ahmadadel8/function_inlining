e2ac93758b48d732583a456a1175ae963036a531
ahmadadel8 <ahmadadelattia@gmail.com>
2021-04-30 23:08:42 +0200

    again

--- sample/lib/Hello/Hello.cpp
+++ sample/lib/Hello/Hello.cpp
@@ -81,7 +81,7 @@ struct Hello :  public FunctionPass
 
 										//auto *ai = new AllocaInst(Type::getInt32Ty(LLVMContext &C)));
 										//auto *dummy_Inst = new Instruction(Type::getInt32Ty(), 0, NULL, 0, *I);
-										// ValueToValueMapTy vmap;
+										 ValueToValueMapTy vmap;
 										// for (inst_iterator callee_I = inst_begin(calleeFunc), callee_E=inst_end(calleeFunc); callee_I!=callee_E; ++callee_I)
 										// 	{ ReturnInst *ri = dyn_cast<ReturnInst>(&*callee_I);
 										// 		if (ri) break;
