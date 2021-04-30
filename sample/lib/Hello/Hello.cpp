6738a616887a3dcf2d7ad0e3f990255828fd4e03
ahmadadel8 <ahmadadelattia@gmail.com>
2021-04-30 21:42:32 +0200

    I->eraseFromParent(); RF_IgnoreMissingLocals still not recognized

--- sample/lib/Hello/Hello.cpp
+++ sample/lib/Hello/Hello.cpp
@@ -89,9 +89,9 @@ struct Hello :  public FunctionPass
 												new_Inst->insertBefore(&*I);
 												vmap[&*callee_I] = new_Inst;
 												llvm::RemapInstruction(new_Inst, vmap,
-								                          RF_NoModuleLevelChanges | RF_IgnoreMissingLocals);
+								                           RF_NoModuleLevelChanges);
 											}
-
+										I->eraseFromParent();
 										// for (inst_iterator callee_I = inst_begin(calleeFunc), callee_E=inst_end(calleeFunc); callee_I!=callee_E; ++callee_I){
 										//   Instruction* temp = callee_I->clone();
 										//
