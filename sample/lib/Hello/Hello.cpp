41898520f9cf5d240fd0b8095f02289eb65cb00a
ahmadadel8 <ahmadadelattia@gmail.com>
2021-04-30 19:28:46 +0200

    auto *ai = new AllocaInst(Type::getInt32Ty());

--- sample/lib/Hello/Hello.cpp
+++ sample/lib/Hello/Hello.cpp
@@ -89,8 +89,8 @@ struct Hello :  public FunctionPass
 										ArgPtr->replaceAllUsesWith(constArg);
 										}
 									actualArgVector.clear();
-									//auto *ai = new AllocaInst(Type::Int32Ty);
-									auto *dummy_Inst = new Instruction(Type::getInt32Ty, 0, NULL, 0, *I);
+									auto *ai = new AllocaInst(Type::getInt32Ty());
+									//auto *dummy_Inst = new Instruction(Type::getInt32Ty, 0, NULL, 0, *I);
 									// ValueToValueMapTy vmap;
 									// for (inst_iterator callee_I = inst_begin(calleeFunc), callee_E=inst_end(calleeFunc); callee_I!=callee_E; ++callee_I)
 									// 	{
