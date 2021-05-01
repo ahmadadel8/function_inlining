b540852dcd406aeda590899d09adecb7ac517b44
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-02 01:50:24 +0200

    trying again

--- sample/lib/Function_Inlining/Function_Inlining.cpp
+++ sample/lib/Function_Inlining/Function_Inlining.cpp
@@ -34,27 +34,37 @@ struct Function_Inlining :  public FunctionPass
 					ReturnInst* retInst;
 					Instruction* calleeInst;
 					//Miscellaneous
+				  Value* actualArg; //A value type variable that iteratively holds the actual arguments
 					std::vector<ConstantInt*> actualArgVector; //A vector that holds the arguments if they are constants.
-				  Value* actualArg;
-					Value* retVal;
-					Value* retPtr;
+					Value* strVal; //The value in the store instruction replacing the call instruction.
+					Value* strPtr; //The return in the store instruction replacing the call instruction.
 					ConstantInt* constArg; //A ConstInt type variable that holds constant arguments after casting from Value to ConstInt
-				  unsigned numArgs;
-					bool areArgsConst;
+				  unsigned numArgs; //number of arguments in a call instruction
+					bool areArgsConst; //a flag. True if all arguments of a call instruction are constants.
+
+
+//First, we need to iterate over all the instructions in the code, until we find a call instruction
 
 					for (inst_iterator I = inst_begin(callerFunc), E=inst_end(callerFunc); I!=E; ++I)
 					{
-						callInst = dyn_cast<CallInst>(&*I);
-						if (callInst){
-							areArgsConst= true;
-							numArgs=callInst->getNumArgOperands();
+						//tries to cast every instruction to callInst class. Returns NULL if not a callInst
+						if (callInst = dyn_cast<CallInst>(&*I)){ //if callInst is not NULL, i.e. intruction is indeed a call instruction
+							//Now that we found the call instruction, we need to check if all the argments are indeed constants.
+							areArgsConst= true; //initialize the flag to be true. It will be set to false if we encounter a non const argument
+							numArgs=callInst->getNumArgOperands(); //the number of the arguments of the all instruction so we can iterate over them
 							for (unsigned ArgIdx=0; ArgIdx<numArgs; ++ArgIdx){
-								actualArg=callInst->getArgOperand(ArgIdx);
-								if(!isa<Constant>(actualArg)) {
+								actualArg=callInst->getArgOperand(ArgIdx); //checks one argument at a time
+
+								if(constArg=dyn_cast<ConstantInt>(actualArg)) 	actualArgVector.push_back(actualArg);
+								else
+								{ //tries to cast
 									areArgsConst= false;
 									break;}
 
-								else actualArgVector.push_back(cast<ConstantInt>(actualArg));
+								// if(!isa<Constant>(actualArg)) { //tries to cast
+								// 	areArgsConst= false;
+								// 	break;}
+								// else actualArgVector.push_back(actualArg);
 								}
 								if (areArgsConst){
 									calleeFunc=callInst->getCalledFunction();
@@ -63,7 +73,7 @@ struct Function_Inlining :  public FunctionPass
 										unsigned Idx=0;
 										for (Function::arg_iterator ArgPtr = calleeFunc->arg_begin(), ArgEnd= calleeFunc->arg_end(); ArgPtr !=ArgEnd; ++ArgPtr){
 											constArg = cast<ConstantInt>(actualArgVector[Idx++]);
-											ArgPtr->replaceAllUsesWith(actualArgVector[Idx++]);
+											ArgPtr->replaceAllUsesWith(constArg);
 											}
 										actualArgVector.clear();
 
@@ -91,9 +101,9 @@ struct Function_Inlining :  public FunctionPass
 																	I++->eraseFromParent();
 																	StoreInst* caller_stInst=dyn_cast<StoreInst>(&*I);
 																	if(caller_stInst){
-																		retVal=strInst->getValueOperand();
-																		retPtr=caller_stInst->getPointerOperand();
-																		StoreInst *str= new StoreInst(retVal, retPtr, &*I);
+																		strVal=strInst->getValueOperand();
+																		strPtr=caller_stInst->getPointerOperand();
+																		StoreInst *str= new StoreInst(strVal, strPtr, &*I);
 																		I++->eraseFromParent();
 																		calleeInst->eraseFromParent();
 																		break;
