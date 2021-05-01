09839f63b9d5eccfe05a056101c0358a86ca8af4
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-01 16:11:40 +0200

    debugging

--- sample/lib/Hello/Hello.cpp
+++ sample/lib/Hello/Hello.cpp
@@ -109,7 +109,7 @@ struct Hello :  public FunctionPass
 										  strInst= dyn_cast<StoreInst>(&*I);
 											if(strInst){
 										  Value* retPtr=strInst->getPointerOperand();
-											errs()<<"ERROR HERE. VALUE"<<*ret<<", PTR" <<*retPtr;
+											 errs()<<"ERROR HERE. VALUE"<<*ret<<", PTR" <<*retPtr;
 										  StoreInst(ret, retPtr, &*I);
 											errs()<<"or tHeRE\n";
 										  I++->eraseFromParent();}}
