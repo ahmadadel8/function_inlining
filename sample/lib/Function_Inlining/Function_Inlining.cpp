0ea1e4417397b0df11963066c68d8c3c807f0fae
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-02 06:52:32 +0200

    may be that's it?

--- sample/lib/Function_Inlining/Function_Inlining.cpp
+++ sample/lib/Function_Inlining/Function_Inlining.cpp
@@ -104,15 +104,16 @@ struct Function_Inlining :  public FunctionPass
 												vmap[&*callee_I] = calleeInst; //then we remap the instructions to update the dominator tree(not sure)
 												RemapInstruction(calleeInst, vmap, RF_NoModuleLevelChanges);
 									}
-									for(lookahead_iterator=I; lookahead_iterator!=E; lookahead_iterator++){
-										errs()<<"in the loop \n";
-										vmap[&*lookahead_iterator] = &*lookahead_iterator;
-										RemapInstruction(&*lookahead_iterator, vmap, RF_NoModuleLevelChanges);}
-										errs()<<"out of the loop \n";
+	
 
 								}
 							}
 						}
+						for(lookahead_iterator=I; lookahead_iterator!=E; lookahead_iterator++){
+							errs()<<"in the loop \n";
+							vmap[&*lookahead_iterator] = &*lookahead_iterator;
+							RemapInstruction(&*lookahead_iterator, vmap, RF_NoModuleLevelChanges);}
+							errs()<<"out of the loop \n";
 					}
 					return true; //return true as the pass has changed the file
 				}
