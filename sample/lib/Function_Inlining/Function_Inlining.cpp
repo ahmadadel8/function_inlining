285308510f11217c4ea4efc2d32de4faad3e0614
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-03 04:31:06 +0200

    Final Commit

--- sample/lib/Function_Inlining/Function_Inlining.cpp
+++ sample/lib/Function_Inlining/Function_Inlining.cpp
@@ -107,7 +107,7 @@ struct Function_Inlining :  public FunctionPass
 												RemapInstruction(calleeInst, vmap, RF_NoModuleLevelChanges);
 									}
 									ReplaceInstWithValue(i->getInstList(), I, retValue); //replaces the call instruction with the return value of the callee and deletes it
-									for (BasicBlock::iterator lookahead_iterator = I; lookahead_iterator!= E; ++lookahead_iterator){
+									for (BasicBlock::iterator lookahead_iterator = I; lookahead_iterator!= E; ++lookahead_iterator)
 										RemapInstruction(lookahead_iterator, vmap, RF_NoModuleLevelChanges);
 
 
