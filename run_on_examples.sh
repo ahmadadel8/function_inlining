2497ee2d33ee86ebd796fc81c2f875d7a08ba36e
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-03 06:59:59 +0200

    updating readme

--- run_on_examples.sh
+++ run_on_examples.sh
@@ -30,12 +30,12 @@ llvm-dis ~/project/test_codes/Function-inlining/example.3inline.constprop.bc -o 
 llvm-dis ~/project/test_codes/Function-inlining/example.4inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.4inline.constprop.ll
 llvm-dis ~/project/test_codes/Function-inlining/example.5inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.5inline.constprop.ll
 
-opt -dce ~/project/test_codes/Function-inlining/example.0inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.0inline.constprop.dce.bc
-opt -dce ~/project/test_codes/Function-inlining/example.1inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.1inline.constprop.dce.bc
-opt -dce ~/project/test_codes/Function-inlining/example.2inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.2inline.constprop.dce.bc
-opt -dce ~/project/test_codes/Function-inlining/example.3inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.3inline.constprop.dce.bc
-opt -dce ~/project/test_codes/Function-inlining/example.4inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.4inline.constprop.dce.bc
-opt -dce ~/project/test_codes/Function-inlining/example.5inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.5inline.constprop.dce.bc
+opt -dce ~/project/test_codes/Function-inlining/example.0inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.0inline.constprop.dce.bc
+opt -dce ~/project/test_codes/Function-inlining/example.1inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.1inline.constprop.dce.bc
+opt -dce ~/project/test_codes/Function-inlining/example.2inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.2inline.constprop.dce.bc
+opt -dce ~/project/test_codes/Function-inlining/example.3inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.3inline.constprop.dce.bc
+opt -dce ~/project/test_codes/Function-inlining/example.4inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.4inline.constprop.dce.bc
+opt -dce ~/project/test_codes/Function-inlining/example.5inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.5inline.constprop.dce.bc
 
 llvm-dis ~/project/test_codes/Function-inlining/example.0inline.constprop.dce.bc -o ~/project/test_codes/Function-inlining/example.0inline.constprop.dce.ll
 llvm-dis ~/project/test_codes/Function-inlining/example.1inline.constprop.dce.bc -o ~/project/test_codes/Function-inlining/example.1inline.constprop.dce.ll
