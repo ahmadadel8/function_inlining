15d8008e2592ee4a2be0085965b92cdb3c0f84a8
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-03 06:31:13 +0200

    adding dead code elimination

--- run_on_examples.sh
+++ run_on_examples.sh
@@ -30,6 +30,14 @@ llvm-dis ~/project/test_codes/Function-inlining/example.3inline.constprop.bc -o 
 llvm-dis ~/project/test_codes/Function-inlining/example.4inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.4inline.constprop.ll
 llvm-dis ~/project/test_codes/Function-inlining/example.5inline.constprop.bc -o ~/project/test_codes/Function-inlining/example.5inline.constprop.ll
 
+opt -dce ~/project/test_codes/Function-inlining/example.0inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.0inline.constprop.dce.ll
+opt -dce ~/project/test_codes/Function-inlining/example.1inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.1inline.constprop.dce.ll
+opt -dce ~/project/test_codes/Function-inlining/example.2inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.2inline.constprop.dce.ll
+opt -dce ~/project/test_codes/Function-inlining/example.3inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.3inline.constprop.dce.ll
+opt -dce ~/project/test_codes/Function-inlining/example.4inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.4inline.constprop.dce.ll
+opt -dce ~/project/test_codes/Function-inlining/example.5inline.constprop.ll -o ~/project/test_codes/Function-inlining/example.5inline.constprop.dce.ll
+
+
 git add ~/project/test_codes/Function-inlining/example.0inline.ll
 git add ~/project/test_codes/Function-inlining/example.1inline.ll
 git add ~/project/test_codes/Function-inlining/example.2inline.ll
@@ -44,6 +52,15 @@ git add ~/project/test_codes/Function-inlining/example.2inline.constprop.ll
 git add ~/project/test_codes/Function-inlining/example.3inline.constprop.ll
 git add ~/project/test_codes/Function-inlining/example.4inline.constprop.ll
 git add ~/project/test_codes/Function-inlining/example.5inline.constprop.ll
+
+git add ~/project/test_codes/Function-inlining/example.0inline.constprop.dce.ll
+git add ~/project/test_codes/Function-inlining/example.1inline.constprop.dce.ll
+git add ~/project/test_codes/Function-inlining/example.2inline.constprop.dce.ll
+git add ~/project/test_codes/Function-inlining/example.3inline.constprop.dce.ll
+git add ~/project/test_codes/Function-inlining/example.4inline.constprop.dce.ll
+git add ~/project/test_codes/Function-inlining/example.5inline.constprop.dce.ll
+
+
 
 git commit -m "uploading test resutls"
 git push origin main
