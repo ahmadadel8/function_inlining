96a8c1cc2a73901407888a58bd319248309cb995
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-03 02:59:18 +0200

    cloning into ai

--- sample/lib/Function_Inlining/Function_Inlining.cpp
+++ sample/lib/Function_Inlining/Function_Inlining.cpp
@@ -106,11 +106,12 @@ struct Function_Inlining :  public FunctionPass
 								{//for ret void, getNumOperands returns 0.
 										break;}
 								calleeInst = callee_I->clone(); //Now, for a normal instruction, we first clone int
-								calleeInst->insertBefore(&*I); //then move it just before the call instruction
+								calleeInst->insertBefore(ai); //then move it just before the call instruction
 								//&*I->getParent()->getInstList().insert(&*I,&*calleeInst); //this is an alternative way to do so that will also work
 								vmap[&*callee_I] = calleeInst; //then we remap the instructions to update the dominator tree(not sure)
 								RemapInstruction(calleeInst, vmap, RF_NoModuleLevelChanges);
 					}
+
 						}
 					return true; //return true as the pass has changed the file
 				}
