58a4fa265abf2c5c2ea66fce83208db4ef5edc41
ahmadadel8 <ahmadadelattia@gmail.com>
2021-05-03 04:06:36 +0200

    fixing the map

--- sample/lib/Function_Inlining/Function_Inlining.cpp
+++ sample/lib/Function_Inlining/Function_Inlining.cpp
@@ -114,9 +114,9 @@ struct Function_Inlining :  public FunctionPass
 								}
 							}
 						}
-						// for(lookahead_iterator=inst_begin(callerFunc); lookahead_iterator!=E; lookahead_iterator++){
+						 for(lookahead_iterator=inst_begin(callerFunc); lookahead_iterator!=E; lookahead_iterator++){
 						// 	vmap[&*lookahead_iterator] = &*lookahead_iterator;
-						// 	RemapInstruction(&*lookahead_iterator, vmap, RF_NoModuleLevelChanges);}
+							RemapInstruction(&*lookahead_iterator, vmap, RF_NoModuleLevelChanges);}
 					}
 					return true; //return true as the pass has changed the file
 				}
